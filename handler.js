00a923bd619e1b605119ffd0eb81aef9f184cc2d
ThienJP <nhj@gmx.net>
2018-10-02 21:14:53 +0200

    initial upload

---
+++ handler.js
@@ -0,0 +1,140 @@
+$("#bakemonoga").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/bakemonoga.mp3']}).play();
+});
+
+$("#diotalk").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/diotalk.mp3']}).play();
+});
+
+$("#douunderstand").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/douunderstand.mp3']}).play();
+});
+
+$("#emeraldsplash").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/emeraldsplash.mp3']}).play();
+});
+
+$("#hell2u").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/hell2u.mp3']}).play();
+});
+
+$("#heybaby").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/heybaby.mp3']}).play();
+});
+
+$("#jaggurs").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/jaggurs.mp3']}).play();
+});
+
+$("#jojo").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/jojo.mp3']}).play();
+});
+
+$("#konodioda").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/konodioda.mp3']}).play();
+});
+
+$("#laliho").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/laliho.mp3']}).play();
+});
+
+$("#mudamuda").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/mudamuda.mp3']}).play();
+});
+
+$("#mudavsora").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/mudavsora.mp3']}).play();
+});
+
+$("#nice").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/nice.mp3']}).play();
+});
+
+$("#nigerundayo").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/nigerundayo.mp3']}).play();
+});
+
+$("#nozoki").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/nozoki.mp3']}).play();
+});
+
+$("#ohshit").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/ohshit.mp3']}).play();
+});
+
+$("#omg").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/omg.mp3']}).play();
+});
+
+$("#perolo").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/perolo.mp3']}).play();
+});
+
+$("#rerorero").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/rerorero.mp3']}).play();
+});
+
+$("#roadroller").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/roadroller.mp3']}).play();
+});
+
+$("#shinekakyoin").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/shinekakyoin.mp3']}).play();
+});
+
+$("#shizaa").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/shizaa.mp3']}).play();
+});
+
+$("#sonochinosadame").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/sonochinosadame.mp3']}).play();
+});
+
+$("#sonofabitch_jojo").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/sonofabitch_jojo.mp3']}).play();
+});
+
+$("#sonofabitch_kyouma").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/sonofabitch_kyouma.mp3']}).play();
+});
+
+$("#tobecontinued").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/tobecontinued.mp3']}).play();
+});
+
+$("#tuturu").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/tuturu.mp3']}).play();
+});
+
+$("#tuturuokarin").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/tuturuokarin.mp3']}).play();
+});
+
+$("#whatrudoing").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/whatrudoing.mp3']}).play();
+});
+
+$("#wryy").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/wryy.mp3']}).play();
+});
+
+$("#wryypuah").on("click",function(){
+    var sound = new Howl({src: ['./soundfiles/wryypuah.mp3']}).play();
+});
+
+$("#yareyaredaze").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/yareyaredaze.mp3']}).play();
+});
+
+$("#yesiam").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/yesiam.mp3']}).play();
+});
+
+$("#yesyes").on("click",function(){
+	var sound = new Howl({src: ['./soundfiles/yesyes.mp3']}).play();
+});
+
+$("#zawarudo").on("click",function(){
+    var sound = new Howl({src: ['./soundfiles/zawarudo.mp3']}).play();
+});
+
